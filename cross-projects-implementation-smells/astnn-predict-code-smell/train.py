817c4287ff29f6fcaef46f9cc12e4d6de5c24a17
yqx <ibd_hdu@163.com>
2023-01-11 15:00:07 +0800

    my sourceCode

---
+++ cross-projects-implementation-smells/astnn-predict-code-smell/train.py
@@ -0,0 +1,252 @@
+import torch
+import torch.nn as nn
+from gensim.models.word2vec import Word2Vec
+import numpy as np
+import pandas as pd
+import time
+from focalloss import FocalLoss
+from preprocess_data import preprocess_data
+
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, precision_recall_curve
+from model import ASTNN
+
+
+
+EPOCH = 30
+
+projectList = ['Ant','jruby','kafka','mockito','storm','tomcat']
+
+def get_batch(dataset, i, batch_size):
+    return dataset.iloc[i: i + batch_size]
+
+def get_10_fold_cross_train_test_list(project):
+    trainlist = []
+    testlist = []
+    for projectname in projectList:
+        if projectname != project:
+            train_list_path = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/trainlabels.txt"
+            trainlist += open(train_list_path).readlines()
+            train_list_path = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/testlabels.txt"
+            trainlist += open(train_list_path).readlines()
+        else:
+            test_list_path = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/trainlabels.txt"
+            testlist += open(test_list_path).readlines()
+            test_list_path = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/testlabels.txt"
+            testlist += open(test_list_path).readlines()
+    
+    new_trainlist = []
+    new_testlist = []
+    for line in trainlist:
+        #print(line.split('    '))
+
+        if line.split('    ')[1] == '0\n':
+            new_trainlist.append(line.split('    ')[0]+'    '+'0')
+        else:
+            new_trainlist.append(line.split('    ')[0]+'    '+'1')
+    for line in testlist:
+        #print(line.split('    '))
+        if line.split('    ')[1] == '0\n':
+            new_testlist.append(line.split('    ')[0]+'    '+'0')
+        else:
+            new_testlist.append(line.split('    ')[0]+'    '+'1')
+    #print(new_trainlist)
+    return new_trainlist, new_testlist
+
+def init_data_and_model(projectname):
+    print("projectname",projectname)
+
+    train_labels, test_labels = get_10_fold_cross_train_test_list(projectname)
+    #train_labels = "/home/yqx/Desktop/TD-data-process/outDataMethod/"+projectname+"/"+"trainlabels.txt"
+    #test_labels = "/home/yqx/Desktop/TD-data-process/outDataMethod/"+projectname+"/"+"testlabels.txt"
+    
+    
+    metohdfilepathList = []
+    for projectname in projectList:
+        metohdfilepathList.append('/home/yqx/Downloads/DesigniteJava-master/myData/'+ projectname +'/methods/pureJavaMethod')
+
+    w2v, programs = preprocess_data(train_labels, test_labels, metohdfilepathList)
+    
+    print('Reading data...')
+    #w2v = Word2Vec.load('./data/w2v_128').wv
+    embeddings = torch.tensor(np.vstack([w2v.wv.vectors, [0] * 128]))
+
+    #programs = pd.read_pickle('./data/programs.pkl')
+
+    #print("programs",programs)
+    #train_labels = open(train_labels).readlines()
+    #test_labels = open(test_labels).readlines()
+    train_code_name = []
+    test_code_name = []
+    for line in train_labels:
+        train_code_name.append(line.split('    ')[0])
+    for line in test_labels:
+        test_code_name.append(line.split('    ')[0])
+
+    train_set_indexTree = []
+    train_set_label = []
+    test_set_indexTree = []
+    test_set_label = []
+    for i,name in enumerate(programs['name']):
+        if programs['name'][i] in train_code_name and programs['index_tree'][i] != []:
+            train_set_indexTree.append(programs['index_tree'][i])
+            train_set_label.append(programs['label'][i])
+        elif programs['name'][i] in test_code_name and programs['index_tree'][i] != []:
+            test_set_indexTree.append(programs['index_tree'][i])
+            test_set_label.append(programs['label'][i])
+
+    training_data = {"index_tree":train_set_indexTree,"label":train_set_label}
+    training_set = pd.DataFrame(training_data, columns = ['index_tree','label'])
+
+    test_data = {"index_tree":test_set_indexTree,"label":test_set_label}
+    test_set = pd.DataFrame(test_data, columns = ['index_tree','label'])
+    '''
+    training_set.to_pickle('./data/train_programs.pkl')
+    test_set.to_pickle('./data/test_programs.pkl')
+    training_set=pd.read_pickle('./data/train_programs.pkl')
+    test_set=pd.read_pickle('./data/test_programs.pkl')
+    '''
+    validation_set = test_set
+
+    net = ASTNN(output_dim=2,
+            embedding_dim=128, num_embeddings=len(w2v.wv.vectors) + 1, embeddings=embeddings,
+            batch_size=BATCH_SIZE)
+    net.cuda()
+
+    #criterion = nn.CrossEntropyLoss()
+    criterion = FocalLoss().cuda()
+    optimizer = torch.optim.Adamax(net.parameters())
+
+    return net,criterion,optimizer,training_set,validation_set,test_set
+
+
+def train(dataset, backward=True):
+    total_acc = 0.0
+    total_loss = 0.0
+    total = 0.0
+    i = 0
+    y_preds = []
+    y_trues = []
+    
+    batchloss = 0
+    while i < len(dataset):
+        data = get_batch(dataset, i, BATCH_SIZE)
+        input, label = data['index_tree'], torch.Tensor([[0,1] if label==1 else [1,0] for label in data['label']]).cuda()
+        tlabel = [label for label in data['label']]
+        i += BATCH_SIZE
+        
+        #print(" label",label.shape)
+        net.zero_grad()
+        net.batch_size = len(input)
+        
+        output = net(input)
+
+        _, predicted = torch.max(output.data, 1)
+        
+
+        loss = criterion(output, label)
+        batchloss += loss
+        if backward and i%64==0:
+            #print(i)
+            batchloss.backward()
+            #loss.backward(retain_graph = True)
+            optimizer.step()
+            batchloss = 0
+
+        # calc acc
+        #pred = output.data.argmax(1)
+        #correct = pred.eq(label).sum().item()
+        correct = torch.sum(torch.eq(torch.argmax(output, dim=1), torch.argmax(label, dim=1)))
+        total_acc += correct
+        total += len(input)
+        total_loss += loss.item() * len(input)
+        #print("tlabel",tlabel)
+        #print("predicted.tolist()",predicted.tolist())
+        #exit()
+        y_trues += tlabel
+        y_preds += predicted.tolist()
+        r=recall_score(y_trues, y_preds)
+        p=precision_score(y_trues, y_preds)
+        f1=f1_score(y_trues, y_preds)
+        #acc = accuracy_score(y_trues, y_preds)
+        #matrix = confusion_matrix(y_trues, y_preds)
+        
+
+    return total_loss / total, total_acc / total, p, r, f1
+import random
+def Undersampling(training_set):
+    print('training_set',len(training_set['label']))
+    pos = 0
+    neg = 0
+    sample_rate = 0.2
+    posSamples = []
+    negSamples = []
+    selectSamples = []
+    for i,label in enumerate(training_set['label']):
+        #print('label',label)
+        if label == 1:
+            pos+=1
+            posSamples.append([training_set['index_tree'][i],label])
+        else:
+            neg+=1
+            negSamples.append([training_set['index_tree'][i],label])
+    print('sample ratio(pos:neg): ',pos,':',neg)
+    if pos>=neg:
+        slcienum = int(neg*sample_rate)
+        selectSamples = negSamples[:slcienum] + posSamples[:slcienum]
+        print('after sampling(pos:neg): ',len(posSamples[:slcienum]),':',len(negSamples[:slcienum]))
+    elif neg>pos:
+        slcienum = int(pos*sample_rate)
+        selectSamples = negSamples[:slcienum] + posSamples[:slcienum]
+        print('after sampling(pos:neg): ',len(posSamples[:slcienum]),':',len(negSamples[:slcienum]))
+    random.shuffle(selectSamples)
+    index_tree = []
+    label = []
+    for item in selectSamples:
+        index_tree.append(item[0])
+        label.append(item[1])
+    data = {'index_tree':index_tree, 'label':label}
+    Sampled_set = pd.DataFrame(data,columns = ['index_tree','label'])
+    return Sampled_set
+
+    
+if __name__ == '__main__':
+    
+
+    #三次求平均值
+    for projectname in projectList:
+        BATCH_SIZE = 1
+        p_sum = r_sum = f1_sum = 0
+        test_result = '/home/yqx/Downloads/DesigniteJava-master/myData/'+ projectname +'/_method_10_cross_.txt'
+        for i in range(1):
+            net,criterion,optimizer,training_set,validation_set,test_set = init_data_and_model(projectname)
+            print('Start Training...')
+            
+            for epoch in range(EPOCH):
+                start_time = time.time()
+                #print(len(training_set),training_set)
+                training_data = Undersampling(training_set)
+
+                training_loss, training_acc, p, r, f1 = train(training_data)
+                #validation_loss, validation_acc, p, r, f1 = train(validation_set, backward=False)
+
+                end_time = time.time()
+                print('[Epoch: %2d/%2d] Train Loss: %.4f, Train Acc: %.3f, Time Cost: %.3f s, \
+                p=%.4g,r=%.4g,f1=%.4g'
+                    % (epoch + 1, EPOCH, training_loss, training_acc, end_time - start_time, p, r, f1))
+
+                #torch.save(net.state_dict(), './data/params_epoch[%d].pkl' % (epoch + 1))
+
+            test_loss, test_acc, p, r, f1 = train(test_set, backward=False)
+            print('Test Acc: %.3f, p=%.4g, r=%.4g, f1=%.4g' % (test_acc, p, r, f1))
+            test_p_r_f1 = open(test_result, 'a')
+            test_p_r_f1.write(projectname+str(epoch) +" "+ str(p) +" "+ str(r) +" "+ str(f1)+"\n")
+            test_p_r_f1.close()
+            p_sum += p
+            r_sum += r
+            f1_sum += f1
+
+            #torch.save(net.state_dict(), './data/params.pkl')
+            #print('Saved model parameters at', './data/params.pkl')
+        #test_p_r_f1 = open(test_result, 'a')
+        #test_p_r_f1.write(projectname+" 平均值： "+ str(p_sum/3.0) +" "+ str(r_sum/3.0) +" "+ str(f1_sum/3.0)+"\n")
+        #test_p_r_f1.close()
---
+++ within-project-implementation-smells/astnn-predict-code-smell/preprocess_data.py
@@ -0,0 +1,118 @@
+import json
+import javalang
+import pandas as pd
+import util
+from gensim.models.word2vec import Word2Vec
+from collections import Counter
+
+from tqdm import tqdm
+import regex as re
+import os
+import sys
+sys.setrecursionlimit(1000000)
+
+def java_method_to_ast(method):
+    tokens = javalang.tokenizer.tokenize(method)
+    parser = javalang.parser.Parser(tokens)
+    tree = parser.parse_member_declaration()
+    return tree
+
+def preprocess_data(projectname):
+    print('Reading all code to code_list...')
+    #读取所有label到dict
+    print("projectname",projectname)
+
+    train_labels = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/trainlabels.txt"
+    test_labels = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/testlabels.txt"
+    typefilepath = "/home/yqx/Downloads/DesigniteJava-master/myData/"+projectname+"/methods/pureJavaMethod"
+
+    label_dict = {}
+    train_label_file = open(train_labels)
+    #tomcat-10.0.0-org.apache.catalina.session-ManagerBase.java    0
+    for line in train_label_file:
+        label_dict[line.split('    ')[0]] = int(line.split('    ')[1])
+    test_label_file = open(test_labels)
+    for line in test_label_file:
+        label_dict[line.split('    ')[0]] = int(line.split('    ')[1])
+
+    name_list = []
+    ast_list = []
+    label_list = []
+    parse_error = 0
+    for root, ds, files in os.walk(typefilepath):
+        
+        for file in tqdm(files):
+            if file not in label_dict:
+                print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',file)
+                continue
+            codepath = root + '/' + file
+            if os.path.getsize(codepath) > 1024*50:
+                print("文件大于50kb")
+                continue
+            codefile=open(codepath,encoding='utf-8')
+            programtext=codefile.read()
+            lines = codefile.readlines()
+            
+            programtext = re.sub(r'((\/[*]([*].+|[\\n]|\\w|\\d|\\s|[^\\x00-\\xff])+[*]\/))', "", programtext)
+            codefile.close()
+            try:
+                tokens = javalang.tokenizer.tokenize(programtext)
+                parser = javalang.parser.Parser(tokens)
+                tree = parser.parse_member_declaration()
+            except:
+                print('parse error')
+                parse_error+=1
+                continue
+            name_list.append(file)
+            ast_list.append(tree)
+            label_list.append(label_dict[file])
+    print("parse_error",parse_error)   
+    data = {"name":name_list,"ast":ast_list,"label":label_list}
+    programs = pd.DataFrame(data,columns = ['name','ast','label'])
+
+    print('Data size:', len(programs))
+
+    print('Training word embedding...')
+
+    programs['corpus'] = programs['ast'].apply(util.ast2sequence)
+
+    #print("programs",programs)
+    w2v = Word2Vec(programs['corpus'], size=128, workers=16, sg=1, min_count=3)  # use w2v[WORD] to get embedding
+    vocab = w2v.wv.vocab
+
+    # transform ASTNode to tree of index in word2vec model
+    def node_to_index(node):
+        result = [vocab[node.token].index if node.token in vocab else len(vocab)]
+        for child in node.children:
+            result.append(node_to_index(child))
+        return result
+
+
+    # transform ast to trees of index in word2vec model
+    def ast_to_index(ast):
+        blocks = []
+        util.get_ast_nodes(ast, blocks)
+        return [node_to_index(b) for b in blocks]
+
+    print('Transforming ast to embedding index tree...')
+
+    programs['index_tree'] = programs['ast'].apply(ast_to_index)
+
+    #print("programs['code']",programs['code'])
+    #print("programs['ast']",programs['ast'])
+    #exit()
+    programs.pop('ast')
+    programs.pop('corpus')
+    #print("programs",programs)
+
+    w2v.save('./data/w2v_128')
+    print("Saved word2vec model at", './data/w2v_128')
+    programs.to_pickle('./data/programs.pkl')
+    print("Saved processed data at", './data/programs.pkl')
+
+
+
+
+
+
+
